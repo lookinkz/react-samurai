fc6851a44e481fd900d17084d8021a7897561aa7
lookinkz <work@quana.kz>
2023-04-03 16:30:20 +0600

    11 is over

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import Technologies from "./components/Technologies";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="logo192.png" alt="the logo" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="#s">Profile</a>
+        </div>
+        <div>
+          <a href="#s">Messages</a>
+        </div>
+        <div>
+          <a href="#s">News</a>
+        </div>
+        <div>
+          <a href="#s">Music</a>
+        </div>
+        <div>
+          <a href="#s">Setting</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img
+            src="https://images3.alphacoders.com/102/102727.jpg"
+            alt="background"
+          />
+        </div>
+        <div>ava + descritions</div>
+        <div>My posts</div>
+        <div>New post</div>
+        <div>Post1</div>
+        <div>Post2</div>
+      </div>
     </div>
   );
 };
--- src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <div>Ruslan Kuanyshbayev</div>;
-};
-
-export default Footer;
